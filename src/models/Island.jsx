27ac8597b506fc3a0fa1c30f20c5c65b97eb8bb5
ContrerasHDaniel <contrerashdaniel@hotmail.com>
2023-12-29 08:31:56 -0600

    plane previous animation

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -13,9 +13,96 @@ import { useFrame, useThree } from "@react-three/fiber";
 import { a } from "@react-spring/three";
 import islandScene from "../assets/3d/island.glb";
 
-const Island = (props) => {
+const Island = ({ isRotating, setIsRotating, ...props }) => {
   const islandRef = useRef();
-  const { nodes, materials } = useGLTF("/island.glb");
+  const lastX = useRef(0);
+  const rotationSpeed = useRef(0);
+  const dampingFactor = 0.95;
+  const { gl, viewport } = useThree();
+  const { nodes, materials } = useGLTF(islandScene);
+
+  useFrame(() => {
+    if (!isRotating) {
+      rotationSpeed.current *= dampingFactor;
+
+      if (Math.abs(rotationSpeed.current) < 0.001) {
+        rotationSpeed.current = 0;
+      }
+    } else {
+      const rotation = islandRef.current.rotation.y;
+    }
+  });
+
+  const handlePointerDown = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    setIsRotating(true);
+
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+
+    lastX.current = clientX;
+  };
+
+  const handlePointerUp = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    setIsRotating(false);
+
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+
+    const delta = clientX - lastX.current / viewport.width;
+    islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+
+    lastX.current = clientX;
+
+    rotationSpeed.current = delta * 0.01 * Math.PI;
+  };
+
+  const handlePointerMove = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+
+    if (isRotating) {
+      handlePointerUp(e);
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "ArrowLeft") {
+      if (!isRotating) {
+        setIsRotating(true);
+      }
+      islandRef.current.rotation.y += 0.01 * Math.PI;
+    } else if (e.key === "ArrowRight") {
+      if (!isRotating) {
+        setIsRotating(true);
+      }
+      islandRef.current.rotation.y -= 0.01 * Math.PI;
+    }
+  };
+
+  const handleKeyUp = (e) => {
+    if (e.key === "ArrowLef" || e.key === "ArrowRight") {
+      setIsRotating(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("pointerup", handlePointerUp);
+    document.addEventListener("pointerdown", handlePointerDown);
+    document.addEventListener("pointermove", handlePointerMove);
+    document.addEventListener("keyup", handleKeyUp);
+    document.addEventListener("keyDown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("pointerup", handlePointerUp);
+      document.removeEventListener("pointerdown", handlePointerDown);
+      document.removeEventListener("pointermove", handlePointerMove);
+      document.removeEventListener("keyup", handleKeyUp);
+      document.removeEventListener("keyDown", handleKeyDown);
+    };
+  }, [gl, handlePointerDown, handlePointerUp, handlePointerMove]);
+
   return (
     <a.group ref={islandRef} {...props}>
       <mesh
